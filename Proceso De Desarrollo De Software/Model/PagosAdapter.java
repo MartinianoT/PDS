c124e0ee77bedef3032200c93f6ea9899c9e573d
ovisdelmal1 <117323969+ovisdelmal1@users.noreply.github.com>
2024-06-21 21:59:23 -0300

    Update PagosAdapter.java

--- Proceso De Desarrollo De Software/Model/PagosAdapter.java
+++ Proceso De Desarrollo De Software/Model/PagosAdapter.java
@@ -10,6 +10,7 @@ public class PagosAdapter extends InterfazPagos {
      * Default constructor
      */
     public PagosAdapter() {
+        this.adaptador = new ServicioExterno();
     }
 
     /**
@@ -21,6 +22,23 @@ public class PagosAdapter extends InterfazPagos {
      * @param factura
      */
     public void procesarPago(Factura factura) {
+        if (factura == null) {
+            throw new IllegalArgumentException("La factura no puede ser nula");
+        }
+        
+        try {
+            boolean resultado = adaptador.procesarPago(factura);
+            
+            if (resultado) {
+                factura.setEstadoPago(true);
+                System.out.println("Pago procesado exitosamente para la factura con cita: " + factura.getCita().getId());
+            } else {
+                System.out.println("Fallo al procesar el pago para la factura con cita: " + factura.getCita().getId());
+            }
+        } catch (Exception e) {
+            System.err.println("Error al procesar el pago: " + e.getMessage());
+        }
+    }
         // TODO implement here
     }
 

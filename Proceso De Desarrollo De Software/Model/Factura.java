af3d80d7381d931f693b8cdb20c9af1131559fc9
MartinianoT <martitrigueiro@gmail.com>
2024-06-21 23:38:12 -0300

    TODAS PUTAS

--- Proceso De Desarrollo De Software/Model/Factura.java
+++ Proceso De Desarrollo De Software/Model/Factura.java
@@ -1,68 +1,79 @@
-
 import java.util.*;
 
-/**
- * 
- */
 public class Factura {
 
-    /**
-     * Default constructor
-     */
-    public Factura() {
-    }
-
-    /**
-     * 
-     */
     private CitaMedica cita;
 
-    /**
-     * 
-     */
     private Double monto;
 
-    /**
-     * 
-     */
     private boolean estadoPago;
 
-    /**
-     * 
-     */
     private Boolean jubilado;
 
-    /**
-     * 
-     */
     private String seguroMedico;
 
-    /**
-     * 
-     */
     private int descuento;
 
-    /**
-     * @return
-     */
-    public void enviarFactura() {
-        // TODO implement here
-        return null;
+    public Factura(CitaMedica cita, Double monto, boolean estadoPago, Boolean jubilado, String seguroMedico,
+            int descuento) {
+        this.cita = cita;
+        this.monto = monto;
+        this.estadoPago = estadoPago;
+        this.jubilado = jubilado;
+        this.seguroMedico = seguroMedico;
+        this.descuento = descuento;
     }
 
-    /**
-     * 
-     */
-    public void <constructor>+Factura(cita: CitaMedica, monto: Double, estadoPago: boolean)() {
-        // TODO implement here
+    public CitaMedica getCita() {
+        return cita;
     }
 
-    /**
-     * @return
-     */
+    public void setCita(CitaMedica cita) {
+        this.cita = cita;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public boolean isEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(boolean estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+
+    public Boolean getJubilado() {
+        return jubilado;
+    }
+
+    public void setJubilado(Boolean jubilado) {
+        this.jubilado = jubilado;
+    }
+
+    public String getSeguroMedico() {
+        return seguroMedico;
+    }
+
+    public void setSeguroMedico(String seguroMedico) {
+        this.seguroMedico = seguroMedico;
+    }
+
+    public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
     public CostoStrategy obtenerMontoFinal() {
-        // TODO implement here
-        return null;
+        return CostoStrategy();
     }
 
 }
